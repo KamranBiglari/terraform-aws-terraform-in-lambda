9a689912b89d08ee303d034436ac6b9248fababf
Kamran Biglari <kamran.biglari@newchangefx.com>
2025-12-19 15:24:17 +0000

    Change variable value to default in variables.tf

--- variables.tf
+++ variables.tf
@@ -60,7 +60,7 @@ variable "function_vpc_subnet_ids" {
 
 variable "function_cloudwatch_logs_retention_in_days" {
   description = "Specifies the number of days you want to retain log events in the specified log group. Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, and 3653."
-  value = 30
+  default = 30
 }
 
 variable "create_role" {
